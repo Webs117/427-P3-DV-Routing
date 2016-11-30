270f56884150e38b4b135486bcba5d5b6297f14c
gmccormi <gmccormi@umich.edu>
2016-11-30 15:51:49 -0500

    project directory setup

---
+++ BellmanFord/src/bellmanford/BellmanFord.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bellmanford;
+
+/**
+ *
+ * @author aggiacca
+ */
+public class BellmanFord {
+    String temp;
+    
+    //constructor
+    public BellmanFord(String temp2){
+        temp = temp2;
+    }
+    
+    //main bellman ford algorithm 
+    public void BellmanFordEval(){
+        
+    }
+}
