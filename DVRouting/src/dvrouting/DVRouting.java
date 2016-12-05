fa4ddac62260e17799c1b21adc3ba5391035408e
Webs117 <adamgcog@gmai.com>
2016-12-04 19:47:40 -0500

    Update
    -Link change and Initial Link

--- DVRouting/src/dvrouting/DVRouting.java
+++ DVRouting/src/dvrouting/DVRouting.java
@@ -154,22 +154,37 @@ public class DVRouting {
                             i = neighborLinks.size();
                         }
                     }
+                    
                     //run Bellman with routerLinks table with current DV table 
-                    //routerTable = BellmanFordEval(routerTable, neighborLinks);
-                    //send 
+                    ArrayList<ArrayList<String>> prevRouterTable = routerTable;
+
+                    //run bellman ford
+                    routerTable = BellmanFord.BellmanFordEval(routerTable, neighborLinks);
+
+                    if (compareTables(routerTable, prevRouterTable)) {
+
+                        String updatedTable = convertArrToStr(routerTable);
+                        byte[] outPacketData = new byte[2048];
+                        outPacketData = updatedTable.getBytes();
+                        InetAddress routerIp = InetAddress.getLocalHost();
+
+                        for (int i = 0; i < neighborPorts.size(); i++)
+                        {
+                        // send updated table to all neighbors
+                        DatagramPacket updatedRouter = new DatagramPacket(outPacketData, outPacketData.length, routerIp, Integer.parseInt(neighborPorts.get(i).get(1)));
+                        UDPclientSocket.send(updatedRouter);
+                        }        
+                    } 
+                    
                     
                 }
-
-                // routerTable.size() send table after link updates 
-                
-                //When links update compare routerLink table with routerBV table 
                 
             }else{
                 
                 ArrayList<ArrayList<String>> neighborTable = convertStrTo2D(packetStr);
                 ArrayList<ArrayList<String>> prevRouterTable = routerTable;
                 
-                //run ballman ford
+                //run bellman ford
                 routerTable = BellmanFord.BellmanFordEval(neighborTable, routerTable);
              
                 if (compareTables(routerTable, prevRouterTable)) {
